8c4285a956493d6a71597b77dcc5de047742cf38
ZrcLeibniz <2834511920@qq.com>
2020-06-17 17:09:40 +0800

    练习

---
+++ Practice/src/zrc/sort/pratice/selection/Selection01.java
@@ -0,0 +1,29 @@
+package zrc.sort.pratice.selection;
+
+import sun.plugin.com.event.COMEventHandler;
+
+public class Selection01 {
+    public static void sort(Comparable[] a){
+        int min;
+        for (int i = 0; i < a.length - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (greater(a[min], a[j])){
+                    min = j;
+                }
+            }
+            exchange(a, min, i);
+        }
+    }
+
+    private static boolean greater(Comparable v, Comparable w){
+        return v.compareTo(w) > 0;
+    }
+
+    private static void exchange(Comparable[] a, int i, int j){
+        Comparable temp;
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
---
+++ Practice/src/zrc/test/sort/SelectionTest.java
@@ -0,0 +1,14 @@
+package zrc.test.sort;
+
+import zrc.sort.Selection;
+import zrc.sort.pratice.selection.Selection01;
+
+import java.util.Arrays;
+
+public class SelectionTest {
+    public static void main(String[] args) {
+        Integer[] arr = {4, 6, 8, 7, 9, 2, 10, 1};
+        Selection01.sort(arr);
+        System.out.println(Arrays.asList(arr));
+    }
+}
