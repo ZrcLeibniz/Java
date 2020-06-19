c3646934aa6403627a7f12b2c35ed2e1f933bbce
ZrcLeibniz <2834511920@qq.com>
2020-06-19 14:33:53 +0800

    练习

---
+++ Practice/src/zrc/sort/pratice/bubble/Bubble04.java
@@ -0,0 +1,24 @@
+package zrc.sort.pratice.bubble;
+
+public class Bubble04 {
+    public static void sort(Comparable[] a){
+        for (int i = a.length - 1; i > 0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                if (greater(a[j], a[j + 1])){
+                    exchange(a, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static boolean greater(Comparable v, Comparable w){
+        return v.compareTo(w) > 0;
+    }
+
+    public static void exchange(Comparable[] a, int i, int j){
+        Comparable temp;
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
