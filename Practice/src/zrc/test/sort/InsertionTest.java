78f81d7f8482ce57bcd10e2cdd8321e20d1e321a
ZrcLeibniz <2834511920@qq.com>
2020-06-22 07:33:26 +0800

    练习

--- Practice/src/zrc/test/sort/InsertionTest.java
+++ Practice/src/zrc/test/sort/InsertionTest.java
@@ -1,9 +1,6 @@
 package zrc.test.sort;
 
-import zrc.sort.pratice.Insertion.Insertion01;
-import zrc.sort.pratice.Insertion.Insertion02;
-import zrc.sort.pratice.Insertion.Insertion03;
-import zrc.sort.pratice.Insertion.Insertion04;
+import zrc.sort.pratice.Insertion.*;
 
 import java.util.Arrays;
 
@@ -11,7 +8,7 @@ public class InsertionTest {
 
     public static void main(String[] args) {
         Integer[] arr = {4, 3, 2, 10, 12, 1, 5, 6};
-        Insertion04.sort(arr);
+        Insertion05.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
