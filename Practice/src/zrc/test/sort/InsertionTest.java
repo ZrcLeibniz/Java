12f498556acb63e509bdb72274b0c303fd3a649a
ZrcLeibniz <2834511920@qq.com>
2020-06-28 09:04:57 +0800

    练习

--- Practice/src/zrc/test/sort/InsertionTest.java
+++ Practice/src/zrc/test/sort/InsertionTest.java
@@ -8,7 +8,7 @@ public class InsertionTest {
 
     public static void main(String[] args) {
         Integer[] arr = {4, 3, 2, 10, 12, 1, 5, 6};
-        Insertion10.sort(arr);
+        Insertion11.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
--- Practice/src/zrc/test/sort/QuickTest.java
+++ Practice/src/zrc/test/sort/QuickTest.java
@@ -1,17 +1,14 @@
 package zrc.test.sort;
 
 import zrc.sort.Quick;
-import zrc.sort.pratice.quick.Quick01;
-import zrc.sort.pratice.quick.Quick02;
-import zrc.sort.pratice.quick.Quick03;
-import zrc.sort.pratice.quick.Quick04;
+import zrc.sort.pratice.quick.*;
 
 import java.util.Arrays;
 
 public class QuickTest {
     public static void main(String[] args) {
         Integer[] arr = {6, 1, 2, 7, 9, 3, 4, 5, 8};
-        Quick04.sort(arr);
+        Quick05.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
