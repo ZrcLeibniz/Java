4c2f4e6c1a09d6cd2b0fb134d8c017def86dd51c
ZrcLeibniz <2834511920@qq.com>
2020-06-30 12:46:32 +0800

    练习

--- Practice/src/zrc/test/sort/ShellTest.java
+++ Practice/src/zrc/test/sort/ShellTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class ShellTest {
     public static void main(String[] args) {
         Integer[] arr = {3, 2, 4, 5, 7, 6, 5, 9, 8, 1};
-        Shell09.sort(arr);
+        Shell10.sort(arr);
         System.out.println(Arrays.asList(arr));
     }
 }
