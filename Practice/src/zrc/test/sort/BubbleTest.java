302452c02e4fb33b64643c017199c44bff60dfe8
ZrcLeibniz <2834511920@qq.com>
2020-07-03 08:47:50 +0800

    练习

--- Practice/src/zrc/test/sort/BubbleTest.java
+++ Practice/src/zrc/test/sort/BubbleTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class BubbleTest {
     public static void main(String[] args) {
         Integer[] arr = {4, 5, 6, 3, 2, 1};
-        Bubble13.sort(arr);
+        Bubble14.sort(arr);
         System.out.println(Arrays.asList(arr));
     }
 }
--- Practice/src/zrc/test/sort/QuickTest.java
+++ Practice/src/zrc/test/sort/QuickTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class QuickTest {
     public static void main(String[] args) {
         Integer[] arr = {6, 1, 2, 7, 9, 3, 4, 5, 8};
-        Quick08.sort(arr);
+        Quick09.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
