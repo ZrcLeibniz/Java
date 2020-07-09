f695750d2e513840bd09704bc50b1bc22f011827
ZrcLeibniz <2834511920@qq.com>
2020-07-09 09:42:07 +0800

    List接口特有的方法

---
+++ Basic/Collection/src/zrc/list/ListTest01.java
@@ -0,0 +1,33 @@
+package zrc.list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListTest01 {
+    public static void main(String[] args) {
+        List arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("D");
+        arrayList.add(0, "King");
+        arrayList.add("King");
+        Iterator iterator = arrayList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        System.out.println(arrayList.get(0));
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+        System.out.println(arrayList.indexOf("King"));
+        System.out.println(arrayList.lastIndexOf("King"));
+        System.out.println(arrayList.remove(0));
+        arrayList.set(1, "king");
+        Iterator iterator1 = arrayList.iterator();
+        while (iterator1.hasNext()) {
+            System.out.println(iterator1.next());
+        }
+    }
+}
