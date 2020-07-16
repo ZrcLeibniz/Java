1ede2598b3275a49b722dc6ef5aca3aa9b3eadf3
ZrcLeibniz <2834511920@qq.com>
2020-07-16 08:36:21 +0800

    冒泡排序练习

--- Practice/src/zrc/test/sort/BubbleTest.java
+++ Practice/src/zrc/test/sort/BubbleTest.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class BubbleTest {
     public static void main(String[] args) {
         Integer[] arr = {4, 5, 6, 3, 2, 1};
-        Bubble16.sort(arr);
+        Bubble17.sort(arr);
         System.out.println(Arrays.asList(arr));
     }
 }
