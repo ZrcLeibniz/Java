7d02d0bceaf44ee58a8a3fc47701bead36e634eb
ZrcLeibniz <2834511920@qq.com>
2020-07-16 09:19:28 +0800

    希尔排序练习

--- Practice/src/zrc/test/sort/ShellTest.java
+++ Practice/src/zrc/test/sort/ShellTest.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class ShellTest {
     public static void main(String[] args) {
         Integer[] arr = {3, 2, 4, 5, 7, 6, 5, 9, 8, 1};
-        Shell13.sort(arr);
+        Shell14.sort(arr);
         System.out.println(Arrays.asList(arr));
     }
 }
