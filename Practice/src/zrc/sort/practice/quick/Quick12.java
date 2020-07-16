e2f55e277ab02131fbf783866e212d0578d57f10
ZrcLeibniz <2834511920@qq.com>
2020-07-16 09:09:45 +0800

    快速排序练习

---
+++ Practice/src/zrc/sort/practice/quick/Quick12.java
@@ -0,0 +1,53 @@
+package zrc.sort.practice.quick;
+
+public class Quick12 {
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exchange(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+        int partition = partition(a, lo, hi);
+        sort(a, lo, partition - 1);
+        sort(a, partition + 1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        Comparable key = a[lo];
+        int left = lo;
+        int right = hi + 1;
+        while (true) {
+            while (less(key, a[--right])) {
+                if (right == lo) {
+                    break;
+                }
+            }
+            while (less(a[++left], key)) {
+                if (left == hi) {
+                    break;
+                }
+            }
+            if (left >= right) {
+                break;
+            } else {
+                exchange(a, left, right);
+            }
+        }
+        exchange(a, lo, right);
+        return right;
+    }
+
+    public static void sort(Comparable[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+        sort(a, lo, hi);
+    }
+}
