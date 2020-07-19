d40f03107a3070e7e75052a22a1ab182a508776b
ZrcLeibniz <2834511920@qq.com>
2020-07-19 09:47:07 +0800

    堆排序练习

---
+++ Practice/src/zrc/sort/practice/heap/Heap02.java
@@ -0,0 +1,53 @@
+package zrc.sort.practice.heap;
+
+public class Heap02<T extends Comparable<T>> {
+    public static void sort(Comparable[] source) {
+        Comparable[] heap = new Comparable[source.length + 1];
+        createHeap(source, heap);
+        int N = heap.length - 1;
+        while (N != 1) {
+            exchange(heap, 1, N);
+            N--;
+            sink(heap, 1, N);
+        }
+        System.arraycopy(heap, 1, source, 0, source.length);
+    }
+
+    private static void createHeap(Comparable[] source, Comparable[] heap) {
+        System.arraycopy(source, 0, heap, 1, source.length);
+        for (int i = heap.length / 2; i > 0; i--) {
+            sink(heap, i, heap.length - 1);
+        }
+    }
+
+    private static void exchange(Comparable[] heap, int i, int j) {
+        Comparable temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+    private static boolean less(Comparable[] heap, int i, int j) {
+        return heap[i].compareTo(heap[j]) < 0;
+    }
+
+    private static void sink(Comparable[] heap, int target, int range) {
+        while (2 * target <= range) {
+            int max;
+            if (2 * target + 1 <= range) {
+                if (less(heap, 2 * target, 2 * target + 1)) {
+                    max = 2 * target + 1;
+                } else {
+                    max = 2 * target;
+                }
+            } else {
+                max = 2 * target;
+            }
+            if (!less(heap, target, max)) {
+                break;
+            } else {
+                exchange(heap, target, max);
+            }
+            target = max;
+        }
+    }
+}
--- Practice/src/zrc/test/sort/HeapTest.java
+++ Practice/src/zrc/test/sort/HeapTest.java
@@ -2,13 +2,14 @@ package zrc.test.sort;
 
 import zrc.sort.Heap;
 import zrc.sort.practice.heap.Heap01;
+import zrc.sort.practice.heap.Heap02;
 
 import java.util.Arrays;
 
 public class HeapTest {
     public static void main(String[] args) {
         String[] arr = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
-        Heap01.sort(arr);
+        Heap02.sort(arr);
         System.out.print(Arrays.asList(arr) + " ");
     }
 }
