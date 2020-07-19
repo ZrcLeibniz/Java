1ce2c465895bc524b46cdba97151afa3ef13c16a
ZrcLeibniz <2834511920@qq.com>
2020-07-19 10:18:54 +0800

    快速排序练习

--- Practice/src/zrc/test/sort/QuickTest.java
+++ Practice/src/zrc/test/sort/QuickTest.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class QuickTest {
     public static void main(String[] args) {
         Integer[] arr = {6, 1, 2, 7, 9, 3, 4, 5, 8};
-        Quick12.sort(arr);
+        Quick13.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
