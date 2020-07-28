1f3227a78526f8470695ff9f6460874563fcb0f5
ZrcLeibniz <2834511920@qq.com>
2020-07-28 15:23:56 +0800

    JZ6

---
+++ Practice/src/zrc/offer/JZ6.java
@@ -0,0 +1,37 @@
+package zrc.offer;
+
+/*
+把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。
+例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。
+NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+ */
+public class JZ6 {
+    public static int minNumberInRotateArray(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+        int lo = 0;
+        int hi = array.length - 1;
+        while (lo < hi) {
+            if (array[lo] < array[hi]) {
+                return array[lo];
+            }
+            int mid = lo + (hi - lo) / 2;
+            if (array[hi] > array[mid]) {
+                hi = mid;
+            } else if (array[hi] < array[mid]) {
+                lo = mid + 1;
+            } else {
+                --hi;
+            }
+        }
+        return array[lo];
+    }
+
+    public static void main(String[] args) {
+        int[] a = {4, 5, 6, 1, 2, 3};
+        int i = minNumberInRotateArray(a);
+        System.out.println(i);
+    }
+}
