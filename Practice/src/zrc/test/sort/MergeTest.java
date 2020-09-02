5dc364584661378cee336147d81eeaa38ccc5b55
ZrcLeibniz <2834511920@qq.com>
2020-09-02 17:22:11 +0800

    归并排序

--- Practice/src/zrc/test/sort/MergeTest.java
+++ Practice/src/zrc/test/sort/MergeTest.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class MergeTest {
     public static void main(String[] args) {
         Integer[] arr = {8, 4, 5, 7, 1, 3, 6, 2};
-        Merge15.sort(arr);
+        Merge16.sort(arr);
         System.out.println(Arrays.asList(arr));
     }
 }
