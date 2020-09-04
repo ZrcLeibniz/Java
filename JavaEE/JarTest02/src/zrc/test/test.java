c2bf3dc09d63a0477c130c3ac25c4fd6d3162c59
ZrcLeibniz <2834511920@qq.com>
2020-09-04 11:41:02 +0800

    jar包的使用

---
+++ JavaEE/JarTest02/src/zrc/test/test.java
@@ -0,0 +1,10 @@
+package zrc.test;
+
+import zrc.entity.Dept;
+
+public class test {
+    public static void main(String[] args) {
+        Dept dept = new Dept();
+        System.out.println(dept);
+    }
+}
