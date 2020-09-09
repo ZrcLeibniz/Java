78a976ecd4d00f971fca6fd712c0881c982fab4d
ZrcLeibniz <2834511920@qq.com>
2020-09-09 09:27:50 +0800

    JDBC

--- JavaEE/JDBC/src/zrc/jdbc/dept/JDBCDept.java
+++ JavaEE/JDBC/src/zrc/jdbc/dept/JDBCDept.java
@@ -11,82 +11,102 @@ public class JDBCDept {
         String sql_2 = "select * from dept";
         String sql_3 = "delete from dept where deptno = ?";
         String sql_4 = "update dept set danme = ?, loc = ? where deptno = ?";
+        String sql_5 = "select count(*) from emp where ename = ? and empno = ?";
         String deptno = "";
         String dname = "";
         String loc = "";
+        String ename = "";
+        String empno = "";
         Class.forName("com.mysql.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/exam of study", "root", "00000000");
         PreparedStatement preparedStatement = null;
-        System.out.println("***欢迎使用XX公司部门管理系统***");
-        System.out.println("***1.添加部门***");
-        System.out.println("***2.查询部门***");
-        System.out.println("***3.删除部门***");
-        System.out.println("***4.更新部门***");
-        System.out.println("请输入操作");
-        flag = scanner.nextInt();
-        if (flag == 1) {
-            System.out.println("***请输入部门的编号***");
-            deptno = scanner.next();
-            System.out.println("***请输入部门的名称***");
-            dname = scanner.next();
-            System.out.println("***请输入部门的位置***");
-            loc = scanner.next();
-            preparedStatement = connection.prepareStatement(sql_1);
-            preparedStatement.setString(1, deptno);
-            preparedStatement.setString(2, dname);
-            preparedStatement.setString(3, loc);
-            int i = preparedStatement.executeUpdate();
-            if (preparedStatement != null) {
+        ResultSet resultSet = null;
+        System.out.println("***请输入用户名***");
+        ename = scanner.next();
+        System.out.println("请输入用户密码");
+        empno = scanner.next();
+        preparedStatement.execute(sql_5);
+        preparedStatement.setString(1, ename);
+        preparedStatement.setString(2, empno);
+        resultSet = preparedStatement.executeQuery();
+        int num = 0;
+        while (resultSet.next()) {
+            num = resultSet.getInt(1);
+        }
+        if (num > 0) {
+            System.out.println("***欢迎使用XX公司部门管理系统***");
+            System.out.println("***1.添加部门***");
+            System.out.println("***2.查询部门***");
+            System.out.println("***3.删除部门***");
+            System.out.println("***4.更新部门***");
+            System.out.println("请输入操作");
+            flag = scanner.nextInt();
+            if (flag == 1) {
+                System.out.println("***请输入部门的编号***");
+                deptno = scanner.next();
+                System.out.println("***请输入部门的名称***");
+                dname = scanner.next();
+                System.out.println("***请输入部门的位置***");
+                loc = scanner.next();
+                preparedStatement = connection.prepareStatement(sql_1);
+                preparedStatement.setString(1, deptno);
+                preparedStatement.setString(2, dname);
+                preparedStatement.setString(3, loc);
+                int i = preparedStatement.executeUpdate();
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (i == 1) {
+                    System.out.println("部门添加成功");
+                } else {
+                    System.out.println("部门添加失败");
+                }
+            } else if (flag == 2) {
+                preparedStatement = connection.prepareStatement(sql_2);
+                resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("deptno") + resultSet.getString("dname") + resultSet.getString("loc"));
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } else if (flag == 3) {
+                System.out.println("请输入要删除的部门编号");
+                int next = scanner.nextInt();
+                preparedStatement = connection.prepareStatement(sql_3);
+                preparedStatement.setInt(1, next);
+                int i = preparedStatement.executeUpdate();
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (i == 1) {
+                    System.out.println("部门删除成功");
+                } else {
+                    System.out.println("部门删除失败");
+                }
+            } else if (flag == 4) {
+                System.out.println("***请输入部门的名称***");
+                dname = scanner.next();
+                System.out.println("***请输入部门的位置***");
+                loc = scanner.next();
+                System.out.println("***请输入部门的编号***");
+                deptno = scanner.next();
+                preparedStatement = connection.prepareStatement(sql_4);
+                preparedStatement.setString(1, dname);
+                preparedStatement.setString(2, loc);
+                preparedStatement.setInt(3, Integer.valueOf(deptno));
+                int i = preparedStatement.executeUpdate();
                 preparedStatement.close();
-            }
-            if (i == 1) {
-                System.out.println("部门添加成功");
+                if (i == 1) {
+                    System.out.println("部门更新成功");
+                } else {
+                    System.out.println("部门更新失败");
+                }
             } else {
-                System.out.println("部门添加失败");
-            }
-        } else if (flag == 2) {
-            preparedStatement = connection.prepareStatement(sql_2);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("deptno") + resultSet.getString("dname") + resultSet.getString("loc"));
-            }
-            if (preparedStatement != null) {
-                preparedStatement.close();
-            }
-        } else if (flag == 3) {
-            System.out.println("请输入要删除的部门编号");
-            int next = scanner.nextInt();
-            preparedStatement = connection.prepareStatement(sql_3);
-            preparedStatement.setInt(1, next);
-            int i = preparedStatement.executeUpdate();
-            if (preparedStatement != null) {
-                preparedStatement.close();
-            }
-            if (i == 1) {
-                System.out.println("部门删除成功");
-            } else {
-                System.out.println("部门删除失败");
-            }
-        } else if (flag == 4) {
-            System.out.println("***请输入部门的名称***");
-            dname = scanner.next();
-            System.out.println("***请输入部门的位置***");
-            loc = scanner.next();
-            System.out.println("***请输入部门的编号***");
-            deptno = scanner.next();
-            preparedStatement = connection.prepareStatement(sql_4);
-            preparedStatement.setString(1, dname);
-            preparedStatement.setString(2, loc);
-            preparedStatement.setInt(3, Integer.valueOf(deptno));
-            int i = preparedStatement.executeUpdate();
-            preparedStatement.close();
-            if (i == 1) {
-                System.out.println("部门更新成功");
-            } else {
-                System.out.println("部门更新失败");
+                System.out.println("请输入有效操作");
             }
         } else {
-            System.out.println("请输入有效操作");
+            System.out.println("用户名或密码错误");
         }
     }
 }
