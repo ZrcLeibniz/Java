80eb29d1a75705535f5e29a83cccbc39fd8911c6
ZrcLeibniz <2834511920@qq.com>
2020-09-15 16:34:38 +0800

    快速排序

--- Practice/src/zrc/test/sort/QuickTest.java
+++ Practice/src/zrc/test/sort/QuickTest.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class QuickTest {
     public static void main(String[] args) {
         Integer[] arr = {6, 1, 2, 7, 9, 3, 4, 5, 8};
-        Quick14.sort(arr);
+        Quick15.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
