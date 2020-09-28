ccced4ab406d8481025620ed5eaa07553a4ba210
ZrcLeibniz <2834511920@qq.com>
2020-09-28 14:52:50 +0800

    ReverseLinkedList

---
+++ LeetCode/struggle/src/zrc/leetcode/ReverseLinkedList.java
@@ -0,0 +1,42 @@
+package zrc.leetcode;
+
+//反转一个单链表。 
+//
+// 示例: 
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL 
+//
+// 进阶: 
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？ 
+// Related Topics 链表 
+// 👍 1243 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class ReverseLinkedListSolution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
+
+
+//leetcode submit region end(Prohibit modification and deletion)
