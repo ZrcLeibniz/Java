48fdcd3daa906ee26974e24e51bd52f3c0fbe321
ZrcLeibniz <2834511920@qq.com>
2020-10-08 14:25:37 +0800

    MergeSortedArray

--- LeetCode/struggle/src/Test.java
+++ LeetCode/struggle/src/Test.java
@@ -1,62 +1,34 @@
 
 public class Test {
     public static void main(String[] args) {
-        ListNode1 listNode1 = new ListNode1(1);
-        ListNode1 listNode2 = new ListNode1(2);
-        ListNode1 listNode3 = new ListNode1(4);
-        ListNode1 listNode4 = new ListNode1(1);
-        ListNode1 listNode5 = new ListNode1(3);
-        ListNode1 listNode6 = new ListNode1(4);
-        listNode1.next = listNode2;
-        listNode2.next = listNode3;
-        listNode4.next = listNode5;
-        listNode5.next = listNode6;
-        ListNode1 listNode11 = mergeTwoLists(listNode1, listNode4);
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        merge(nums1, 3, nums2, 3);
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.println(nums1[i]);
+        }
     }
 
-    public static ListNode1 mergeTwoLists(ListNode1 l1, ListNode1 l2) {
-        ListNode1 p1 = l1;
-        ListNode1 p2 = l2;
-        ListNode1 newNode = new ListNode1();
-        newNode.val = l1.val > l2.val ? l2.val : l1.val;
-        ListNode1 temp = newNode;
-        while (p1 != null && p2 != null) {
-            if (p1.val < p2.val) {
-                temp.next = p1;
-                p1 = p1.next;
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] nums3 = new int[m + n];
+        int p1 = 0;
+        int p2 = 0;
+        int index = 0;
+        while (p1 < m && p2 < n) {
+            if (nums1[p1] < nums2[p2]) {
+                nums3[index++] = nums1[p1++];
             } else {
-                temp.next = p2;
-                p2 = p2.next;
+                nums3[index++] = nums2[p2++];
             }
-            temp = temp.next;
         }
-        while (p1 != null) {
-            temp.next = p1;
-            p1 = p1.next;
-            temp = temp.next;
+        while (p1 < m) {
+            nums3[index++] = nums1[p1++];
         }
-        while (p2 != null) {
-            temp.next = p2;
-            p2 = p2.next;
-            temp = temp.next;
+        while (p2 < n) {
+            nums3[index++] = nums2[p2++];
         }
-        return newNode.next;
-    }
-}
-
-class ListNode1 {
-    int val;
-    ListNode1 next;
-
-    ListNode1() {
-    }
-
-    ListNode1(int val) {
-        this.val = val;
-    }
-
-    ListNode1(int val, ListNode1 next) {
-        this.val = val;
-        this.next = next;
+        for (int i = 0; i < nums3.length; i++) {
+            nums1[i] = nums3[i];
+        }
     }
 }
---
+++ LeetCode/struggle/src/zrc/leetcode/MergeSortedArray.java
@@ -0,0 +1,39 @@
+package zrc.leetcode;
+
+//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。 
+// 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。 
+// 
+//
+// 
+//
+// 示例: 
+//
+// 输入:
+//nums1 = [1,2,3,0,0,0], m = 3
+//nums2 = [2,5,6],       n = 3
+//
+//输出: [1,2,2,3,5,6] 
+// Related Topics 数组 双指针 
+// 👍 644 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class MergeSortedArraySolution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int index = m + n - 1;
+        while (p2 >= 0 && p1 >= 0) {
+            nums1[index--] = nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--];
+        }
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
