418b6e2e1cfb897caf6d3997f386e62f94c16d6a
ZrcLeibniz <2834511920@qq.com>
2020-10-13 10:16:38 +0800

    Mybatis

---
+++ Mybatis/MybatisStudy/mybatis-03/src/main/java/zrc/pojo/Users.java
@@ -0,0 +1,49 @@
+package zrc.pojo;
+
+public class Users {
+    private int id;
+    private String name;
+    private String pwd;
+
+    public Users(int id, String name, String pwd) {
+        this.id = id;
+        this.name = name;
+        this.pwd = pwd;
+    }
+
+    public Users() {
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
