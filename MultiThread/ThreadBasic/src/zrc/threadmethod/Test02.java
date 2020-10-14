79314bcf10246d9465929d4bbc3fbfae96949eff
ZrcLeibniz <2834511920@qq.com>
2020-10-14 10:25:39 +0800

    多线程常用方法currentThread

---
+++ MultiThread/ThreadBasic/src/zrc/threadmethod/Test02.java
@@ -0,0 +1,12 @@
+package zrc.threadmethod;
+
+public class Test02 {
+    public static void main(String[] args) throws InterruptedException {
+        SubThread2 subThread2 = new SubThread2();
+        subThread2.setName("subThread2");
+        subThread2.start();
+        Thread.sleep(5000);
+        Thread thread3 = new Thread(subThread2);
+        thread3.start();
+    }
+}
