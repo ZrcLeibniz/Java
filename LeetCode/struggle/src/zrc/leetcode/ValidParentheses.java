537ef828a6d47e582f9d7a230a5255a335d703f8
ZrcLeibniz <2834511920@qq.com>
2020-10-15 14:51:12 +0800

    ValidParentheses

--- LeetCode/struggle/src/zrc/leetcode/ValidParentheses.java
+++ LeetCode/struggle/src/zrc/leetcode/ValidParentheses.java
@@ -46,25 +46,21 @@ package zrc.leetcode;
 import java.util.Stack;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class ValidParentheseSolution {
+class ValidParenthesesSolution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i <s.length(); i++) {
-            if (stack.isEmpty()) {
-                stack.push(s.charAt(i));
-            } else {
-                if (stack.peek() == s.charAt(i)) {
-                    stack.pop();
-                } else {
-                    stack.push(s.charAt(i));
-                }
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
             }
         }
-        if (stack.isEmpty()) {
-            return true;
-        } else {
-            return false;
-        }
+        return stack.isEmpty();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
