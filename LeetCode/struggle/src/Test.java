f99855b0f2b6e9314f6c8783c2166aa1b5d772b0
ZrcLeibniz <2834511920@qq.com>
2020-10-15 15:35:26 +0800

    MinStack

--- LeetCode/struggle/src/Test.java
+++ LeetCode/struggle/src/Test.java
@@ -2,9 +2,15 @@ import java.util.Stack;
 
 public class Test {
     public static void main(String[] args) {
-        String s = "()";
-        boolean valid = isValid(s);
-        System.out.println(valid);
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        int min = minStack.getMin();
+        minStack.pop();
+        int top = minStack.top();
+        int min1 = minStack.getMin();
+        System.out.println(min + " " + " " + top + " " + min1);
     }
 
     public static boolean isValid(String s) {
@@ -25,5 +31,45 @@ public class Test {
         } else {
             return false;
         }
+    }
+}
+
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(Integer x) {
+        if (stack.isEmpty()) {
+            stack.push(x);
+            minStack.push(x);
+        } else {
+            stack.push(x);
+            if (minStack.peek() <= x) {
+                minStack.push(minStack.peek());
+            } else {
+                minStack.push(x);
+            }
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
     }
 }
