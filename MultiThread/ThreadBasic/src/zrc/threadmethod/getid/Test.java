dcd356ca079fb594def9369abe30fac42687a01e
ZrcLeibniz <2834511920@qq.com>
2020-10-15 10:31:26 +0800

    多线程常用方法getId

--- MultiThread/ThreadBasic/src/zrc/threadmethod/getid/Test.java
+++ MultiThread/ThreadBasic/src/zrc/threadmethod/getid/Test.java
@@ -3,6 +3,7 @@ package zrc.threadmethod.getid;
 public class Test {
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName() + ":" + Thread.currentThread().getId());
+        // 某个编号的线程运行结束后，可能被后续创建的线程使用
         for (int i = 1; i < 5; i++) {
             SubThread5 subThread5 = new SubThread5();
             subThread5.start();
