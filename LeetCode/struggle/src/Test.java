27b1db8572424faa917220d363170e559c4fa19f
ZrcLeibniz <2834511920@qq.com>
2020-10-24 10:03:21 +0800

    C Git Test

--- LeetCode/struggle/src/Test.java
+++ LeetCode/struggle/src/Test.java
@@ -2,9 +2,21 @@ import java.util.Stack;
 
 public class Test {
     public static void main(String[] args) {
-        int[] heights = {2, 1, 5, 6, 2, 3};
-        int i = largestRectangleArea(heights);
-        System.out.println(i);
+        int reverse = reverse(123);
+        System.out.println(reverse);
+    }
+
+    public static int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            if ((ans * 10) / 10 != ans) {
+                ans = 0;
+                break;
+            }
+            ans = ans * 10 + x % 10;
+            x = x / 10;
+        }
+        return ans;
     }
 
     public static int largestRectangleArea(int[] heights) {
