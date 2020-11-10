21d7a6b7c8421a9a8d431e63507745a87d03a154
ZrcLeibniz <2834511920@qq.com>
2020-11-10 12:32:36 +0800

    AOP Spring API

---
+++ SpringStudy/Spring-08-Aop/src/main/java/rich/zrc/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package rich.zrc.serviceImpl;
+
+import rich.zrc.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public void add() {
+        System.out.println("增加了一个用户");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("删除了一个用户");
+    }
+
+    @Override
+    public void update() {
+        System.out.println("修改了一个用户");
+    }
+
+    @Override
+    public void query() {
+        System.out.println("查询了一个用户");
+    }
+}
