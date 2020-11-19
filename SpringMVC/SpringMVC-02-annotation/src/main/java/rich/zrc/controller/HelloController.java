f91f89222b758cad3a85db02125c743d239b7046
ZrcLeibniz <2834511920@qq.com>
2020-11-19 12:28:26 +0800

    SpringMVC Annotation

---
+++ SpringMVC/SpringMVC-02-annotation/src/main/java/rich/zrc/controller/HelloController.java
@@ -0,0 +1,15 @@
+package rich.zrc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("msg", "Hello SpringMVC Annotation");
+        return "Hello";
+    }
+}
