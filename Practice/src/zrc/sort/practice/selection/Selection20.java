bdcd8def872ddd2f37b187ef910918e20336053c
ZrcLeibniz <2834511920@qq.com>
2020-12-02 21:41:45 +0800

    Insertion20

---
+++ Practice/src/zrc/sort/practice/selection/Selection20.java
@@ -0,0 +1,26 @@
+package zrc.sort.practice.selection;
+
+public class Selection20 {
+
+    public static void sort(Comparable[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j <= a.length - 1; j++) {
+                if (greater(a[minIndex], a[j])) {
+                    minIndex = j;
+                }
+            }
+            exchange(a, minIndex, i);
+        }
+    }
+
+    private static boolean greater(Comparable v, Comparable w) {
+        return v.compareTo(w) > 0;
+    }
+
+    private static void exchange(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
