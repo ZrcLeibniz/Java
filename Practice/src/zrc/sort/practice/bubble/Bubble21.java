5769d3424e521644c1ca3376564eefc7d3f8a597
ZrcLeibniz <2834511920@qq.com>
2020-12-02 20:55:59 +0800

    Bubble21

---
+++ Practice/src/zrc/sort/practice/bubble/Bubble21.java
@@ -0,0 +1,24 @@
+package zrc.sort.practice.bubble;
+
+public class Bubble21 {
+
+    public static void sort(Comparable[] a) {
+        for (int i = a.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (greater(a[j], a[j + 1])) {
+                    exchange(a, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static boolean greater(Comparable v, Comparable w) {
+        return v.compareTo(w) > 0;
+    }
+
+    private static void exchange(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
