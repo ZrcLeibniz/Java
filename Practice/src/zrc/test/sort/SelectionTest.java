58efbbefa2c70b4329d634400a043e897583bd8d
ZrcLeibniz <2834511920@qq.com>
2020-12-02 21:17:14 +0800

    Selection18

--- Practice/src/zrc/test/sort/SelectionTest.java
+++ Practice/src/zrc/test/sort/SelectionTest.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class SelectionTest {
     public static void main(String[] args) {
         Integer[] arr = {4, 6, 8, 7, 9, 2, 10, 1};
-        Selection17.sort(arr);
+        Selection18.sort(arr);
         System.out.println(Arrays.asList(arr));
     }
 }
