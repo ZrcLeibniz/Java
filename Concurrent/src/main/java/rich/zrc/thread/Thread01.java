9c8daa97de474c90d617bd4f27877cb9df7336f5
ZrcLeibniz <2834511920@qq.com>
2020-12-09 22:50:24 +0800

    Java并发-创建线程-Thread类

---
+++ Concurrent/src/main/java/rich/zrc/thread/Thread01.java
@@ -0,0 +1,20 @@
+package rich.zrc.thread;
+
+import lombok.extern.slf4j.Slf4j;
+
+// 使用Thread创建线程
+
+@Slf4j(topic = "c.Test")
+public class Thread01 {
+    public static void main(String[] args) {
+        //final Logger logger = Logger.getLogger(Test.class);
+        Thread t1 = new Thread("rich") {
+            @Override
+            public void run() {
+                log.debug("running");
+            }
+        };
+        t1.start();
+        log.debug("running");
+    }
+}
