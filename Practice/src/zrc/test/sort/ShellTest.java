19dfe079d4ad863130266b6779658298f5952dc7
ZrcLeibniz <2834511920@qq.com>
2020-12-11 09:30:07 +0800

    Shell19

--- Practice/src/zrc/test/sort/ShellTest.java
+++ Practice/src/zrc/test/sort/ShellTest.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class ShellTest {
     public static void main(String[] args) {
         Integer[] arr = {3, 2, 4, 5, 7, 6, 9, 8, 1};
-        Shell18.sort(arr);
+        Shell19.sort(arr);
         System.out.println(Arrays.asList(arr));
     }
 }
