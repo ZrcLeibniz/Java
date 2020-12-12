9a83b8b87753fbe6177e05f1572421110649476d
ZrcLeibniz <2834511920@qq.com>
2020-12-12 10:18:30 +0800

    Insertion27

--- Practice/src/zrc/test/sort/InsertionTest.java
+++ Practice/src/zrc/test/sort/InsertionTest.java
@@ -8,7 +8,7 @@ public class InsertionTest {
 
     public static void main(String[] args) {
         Integer[] arr = {4, 3, 2, 10, 12, 1, 5, 6};
-        Insertion26.sort(arr);
+        Insertion27.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
