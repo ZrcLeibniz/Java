99150ccdd1752c88ae6f4d449ee1cc6e0d956434
ZrcLeibniz <2834511920@qq.com>
2020-12-14 09:52:23 +0800

    Insertion28

---
+++ Practice/src/zrc/sort/practice/Insertion/Insertion28.java
@@ -0,0 +1,26 @@
+package zrc.sort.practice.Insertion;
+
+public class Insertion28 {
+
+    public static void sort(Comparable[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (!greater(a[j], a[j - 1])) {
+                    exchange(a, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static boolean greater(Comparable v, Comparable w) {
+        return v.compareTo(w) > 0;
+    }
+
+    private static void exchange(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
--- Practice/src/zrc/test/sort/InsertionTest.java
+++ Practice/src/zrc/test/sort/InsertionTest.java
@@ -8,7 +8,7 @@ public class InsertionTest {
 
     public static void main(String[] args) {
         Integer[] arr = {4, 3, 2, 10, 12, 1, 5, 6};
-        Insertion27.sort(arr);
+        Insertion28.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
