fff8a4549a8e56bf5c05a6332010bc26913bf879
ZrcLeibniz <2834511920@qq.com>
2020-12-28 12:07:14 +0800

    Bubble33

--- Practice/src/zrc/test/sort/BubbleTest.java
+++ Practice/src/zrc/test/sort/BubbleTest.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class BubbleTest {
     public static void main(String[] args) {
         Integer[] arr = {4, 5, 6, 3, 2, 1};
-        Bubble32.sort(arr);
+        Bubble33.sort(arr);
         System.out.println(Arrays.asList(arr));
     }
 }
