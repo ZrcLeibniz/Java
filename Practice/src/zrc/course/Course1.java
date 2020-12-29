c6ad7bb2dd0773cbfb4bae7fd8018b136dc50eb9
ZrcLeibniz <2834511920@qq.com>
2020-12-29 15:25:26 +0800

    算法1

--- Practice/src/zrc/course/Course1.java
+++ Practice/src/zrc/course/Course1.java
@@ -154,7 +154,7 @@ class SmallSum{
         if (arr == null || arr.length < 2) {
             return 0;
         }
-        return 1;
+        return mergeSort(arr, 0, arr.length - 1);
     }
 
     private static int mergeSort(int[] arr, int l, int r) {
@@ -171,9 +171,9 @@ class SmallSum{
         int p1 = l;
         int p2 = mid + 1;
         int result = 0;
-        while (p1 <= mid && p2 <= 2) {
+        while (p1 <= mid && p2 <= r) {
             result += arr[p1] < arr[p2] ? (r - p2 + 1) * arr[p1] : 0;
-            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2]++;
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
         }
         while (p1 <= mid) {
             help[i++] = arr[p1++];
