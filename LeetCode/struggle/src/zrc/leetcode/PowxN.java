29b17fb2021b1281626edef1962cb9dd1e8038f7
ZrcLeibniz <2834511920@qq.com>
2021-01-05 11:02:25 +0800

    PowXN

---
+++ LeetCode/struggle/src/zrc/leetcode/PowxN.java
@@ -0,0 +1,47 @@
+package zrc.leetcode;
+
+//实现 pow(x, n) ，即计算 x 的 n 次幂函数。 
+//
+// 示例 1: 
+//
+// 输入: 2.00000, 10
+//输出: 1024.00000
+// 
+//
+// 示例 2: 
+//
+// 输入: 2.10000, 3
+//输出: 9.26100
+// 
+//
+// 示例 3: 
+//
+// 输入: 2.00000, -2
+//输出: 0.25000
+//解释: 2-2 = 1/22 = 1/4 = 0.25 
+//
+// 说明: 
+//
+// 
+// -100.0 < x < 100.0 
+// n 是 32 位有符号整数，其数值范围是 [−231, 231 − 1] 。 
+// 
+// Related Topics 数学 二分查找 
+// 👍 565 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class PowXNSolution {
+    public double myPow(double x, int n) {
+        return n >= 0 ? quick(x, n) : 1.0 / quick(x, -n);
+    }
+
+    private double quick(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double result = quick(x, n / 2);
+        return n % 2 == 0 ? result * result : result * result * x;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
