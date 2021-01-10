e183983d71d88910821e72b1d1387b7516b5af97
ZrcLeibniz <2834511920@qq.com>
2021-01-10 18:50:10 +0800

    简单的买票练习

---
+++ Concurrent/src/main/java/rich/zrc/monitor/exercise/ExerciseTransfer.java
@@ -0,0 +1,63 @@
+package rich.zrc.monitor.exercise;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
+
+@Slf4j(topic = "c.Test5")
+public class ExerciseTransfer {
+    public static void main(String[] args) {
+        TicketWindow ticketWindow = new TicketWindow(10000);
+        List<Integer> amountList = new Vector<>();
+        List<Thread> threadList = new ArrayList<>();
+        for (int i = 0; i < 2000; i++) {
+            Thread thread = new Thread(() -> {
+                int sell = ticketWindow.sell(randomAmount());
+                amountList.add(sell);
+            });
+            threadList.add(thread);
+            thread.start();
+        }
+        for (Thread thread : threadList) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        log.debug("剩余的票数：{}", ticketWindow.getCount());
+        log.debug("卖出的票数：{}", amountList.stream().mapToInt(i -> i).sum());
+    }
+
+    static Random random = new Random();
+
+    public static int randomAmount() {
+        return random.nextInt(5) + 1;
+    }
+}
+
+class TicketWindow {
+    private int count;
+
+    public TicketWindow(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int sell(int amount) {
+        synchronized (this) {
+            if (this.count >= amount) {
+                this.count -= amount;
+                return amount;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
