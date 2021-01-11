f9fdbbfe7b45eda1ae81c2f6275993bc4747e8d5
ZrcLeibniz <2834511920@qq.com>
2021-01-11 11:42:12 +0800

    KMP

---
+++ Practice/src/zrc/course/course8/KMP.java
@@ -0,0 +1,46 @@
+package zrc.course.course8;
+
+public class KMP {
+    public int getIndexOf(String str, String target) {
+        if (str == null || target == null || target.length() < 1 || str.length() < target.length()) {
+            return -1;
+        }
+        char[] str1 = str.toCharArray();
+        char[] str2 = target.toCharArray();
+        int i1 = 0;
+        int i2 = 0;
+        int[] next = getNextArray(str2);
+        while (i1 < str.length() && i2 < target.length()) {
+            if (str1[i1] == str2[i2]) {
+                i1++;
+                i2++;
+            } else if (next[i2] == -1) {
+                i1++;
+            } else {
+                i2 = next[i2];
+            }
+        }
+        return i2 == target.length() ? i1 - i2 : -1;
+    }
+
+    private int[] getNextArray(char[] str) {
+        if (str.length == 1) {
+            return new int[]{-1};
+        }
+        int[] next = new int[str.length];
+        next[0] = -1;
+        next[1] = 0;
+        int i = 2;
+        int cn = 0;
+        while (i < next.length) {
+            if (str[i - 1] == str[cn]) {
+                next[i++] = ++cn;
+            } else if (cn > 0) {
+                cn = next[cn];
+            } else {
+                next[i++] = 0;
+            }
+        }
+        return next;
+    }
+}
