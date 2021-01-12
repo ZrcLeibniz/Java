5ce20dad213face4d5f5d2a9af34c32ac3853b91
ZrcLeibniz <2834511920@qq.com>
2021-01-12 14:41:05 +0800

    LeetCode 5 LongPalindromicSubstring

---
+++ LeetCode/struggle/src/zrc/leetcode/reback/LongestPalindromicSubstring.java
@@ -0,0 +1,82 @@
+package zrc.leetcode.reback;
+
+//给你一个字符串 s，找到 s 中最长的回文子串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "cbbd"
+//输出："bb"
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "a"
+//输出："a"
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：s = "ac"
+//输出："a"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 仅由数字和英文字母（大写和/或小写）组成 
+// 
+// Related Topics 字符串 动态规划 
+// 👍 3085 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class LongPalindromicSubstringSolution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        int max = 1;
+        int begin = 0;
+        boolean[][] dp = new boolean[len][len];
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (chars[i] != chars[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                if (dp[i][j] && j - i + 1 > max) {
+                    max = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + max);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
