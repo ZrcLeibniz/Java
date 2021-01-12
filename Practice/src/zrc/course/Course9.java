4dd99520b4ba8c8464a306673d1a104120e9a4ff
ZrcLeibniz <2834511920@qq.com>
2021-01-12 10:44:53 +0800

    算法9

--- Practice/src/zrc/course/Course9.java
+++ Practice/src/zrc/course/Course9.java
@@ -253,3 +253,89 @@ class PalindromeString {
         return String.valueOf(res);
     }
 }
+
+class BFPRT {
+    public int getMinKthByBFPRT(int[] arr, int K) {
+        int[] copyArr = copyArray(arr);
+        return bfprt(copyArr, 0, copyArr.length - 1, K - 1);
+    }
+
+    private int[] copyArray(int[] arr) {
+        int[] res = new int[arr.length];
+        for (int i = 0; i != res.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    public int bfprt(int[] arr, int begin, int end, int i) {
+        if (begin == end) {
+            return arr[begin];
+        }
+        int pivot = medianOfMedians(arr, begin, end);
+        int[] pivotRange = partition(arr, begin, end, pivot);
+        if (i >= pivotRange[0] && i <= pivotRange[1]) {
+            return arr[i];
+        } else if (i < pivotRange[0]) {
+            return bfprt(arr, begin, pivotRange[0] - 1, i);
+        } else {
+            return bfprt(arr, pivotRange[1] + 1, end, i);
+        }
+    }
+
+    private int[] partition(int[] arr, int begin, int end, int pivot) {
+        int small = begin - 1;
+        int cur = begin;
+        int big = end + 1;
+        while (cur != big) {
+            if (arr[cur] < pivot) {
+                swap(arr, ++small, cur++);
+            } else if (arr[cur] > pivot) {
+                swap(arr, --big, cur);
+            } else {
+                cur++;
+            }
+        }
+        int[] range = new int[2];
+        range[0] = small + 1;
+        range[1] = big - 1;
+        return range;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private int medianOfMedians(int[] arr, int begin, int end) {
+        int num = end - begin + 1;
+        int offset = num % 5 == 0 ? 0 : 1;
+        int[] mArr = new int[num / 5 + offset];
+        for (int i = 0; i < mArr.length; i++) {
+            int beginI = begin + i * 5;
+            int endI = beginI + 4;
+            mArr[i] = getMedian(arr, beginI, Math.min(end, endI));
+        }
+        return bfprt(mArr, 0, mArr.length - 1, mArr.length / 2);
+    }
+
+    private int getMedian(int[] arr, int begin, int end) {
+        insertionSort(arr, begin, end);
+        int sum = end + begin;
+        int mid = (sum / 2) + (sum % 2);
+        return arr[mid];
+    }
+
+    private void insertionSort(int[] arr, int begin, int end) {
+        for (int i = begin; i <= end; i++) {
+            for (int j = i; j > begin; i--) {
+                if (arr[j - 1] > arr[j]) {
+                    swap(arr, j - 1, j);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
