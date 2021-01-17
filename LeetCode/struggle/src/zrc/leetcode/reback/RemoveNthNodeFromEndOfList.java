e88eaa2395ef1bd7e0e8d6fa3ed11ae44fcf676f
ZrcLeibniz <2834511920@qq.com>
2021-01-17 14:45:10 +0800

    LeetCode 19 RemoveNthNodeFromEndOfList

---
+++ LeetCode/struggle/src/zrc/leetcode/reback/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,71 @@
+package zrc.leetcode.reback;
+
+//给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。 
+//
+// 示例： 
+//
+// 给定一个链表: 1->2->3->4->5, 和 n = 2.
+//
+//当删除了倒数第二个节点后，链表变为 1->2->3->5.
+// 
+//
+// 说明： 
+//
+// 给定的 n 保证是有效的。 
+//
+// 进阶： 
+//
+// 你能尝试使用一趟扫描实现吗？ 
+// Related Topics 链表 双指针 
+// 👍 1176 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class RemoveNthNodeFromEndOfListSolution {
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode pre = new ListNode();
+        pre.next = head;
+        ListNode slow = pre;
+        ListNode fast = pre;
+        while (n != 0) {
+            fast = fast.next;
+            n--;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return pre.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
