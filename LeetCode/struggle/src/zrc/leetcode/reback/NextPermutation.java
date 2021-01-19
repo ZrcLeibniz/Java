14ee460180023cd2e21ef720df4dd2ef7c5f6196
ZrcLeibniz <2834511920@qq.com>
2021-01-19 15:08:28 +0800

    LeetCode 31 NextPermutation

---
+++ LeetCode/struggle/src/zrc/leetcode/reback/NextPermutation.java
@@ -0,0 +1,82 @@
+package zrc.leetcode.reback;
+
+//实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。 
+//
+// 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。 
+//
+// 必须 原地 修改，只允许使用额外常数空间。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[1,3,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,1]
+//输出：[1,2,3]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1,1,5]
+//输出：[1,5,1]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：nums = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 100 
+// 0 <= nums[i] <= 100 
+// 
+// Related Topics 数组 
+// 👍 907 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class NextPermutationSolution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        int j = nums.length - 1;
+        if (i >= 0) {
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int i) {
+        int begin = i;
+        int end = nums.length - 1;
+        while (begin < end) {
+            swap(nums, begin++, end--);
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
