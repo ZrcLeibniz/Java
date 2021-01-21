0b5ddc640c82b38c0cee67b24f8e3f6bb7dde1e4
ZrcLeibniz <2834511920@qq.com>
2021-01-21 16:15:47 +0800

    LeetCode 43 MultiplyStrings

--- LeetCode/struggle/src/zrc/leetcode/PlusOne.java
+++ LeetCode/struggle/src/zrc/leetcode/PlusOne.java
@@ -33,9 +33,9 @@ class PlusOneSolution {
                 return digits;
             }
         }
-        digits = new int[digits.length + 1];
-        digits[0] = 1;
-        return digits;
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
