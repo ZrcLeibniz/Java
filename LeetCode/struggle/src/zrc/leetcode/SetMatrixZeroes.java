37ece09b89a75d7e62af621be68ac55b701b653e
ZrcLeibniz <2834511920@qq.com>
2021-01-22 17:37:35 +0800

    SetMatrixZeros

---
+++ LeetCode/struggle/src/zrc/leetcode/SetMatrixZeroes.java
@@ -0,0 +1,91 @@
+package zrc.leetcode;
+
+//给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。 
+//
+// 示例 1: 
+//
+// 输入: 
+//[
+//  [1,1,1],
+//  [1,0,1],
+//  [1,1,1]
+//]
+//输出: 
+//[
+//  [1,0,1],
+//  [0,0,0],
+//  [1,0,1]
+//]
+// 
+//
+// 示例 2: 
+//
+// 输入: 
+//[
+//  [0,1,2,0],
+//  [3,4,5,2],
+//  [1,3,1,5]
+//]
+//输出: 
+//[
+//  [0,0,0,0],
+//  [0,4,5,0],
+//  [0,3,1,0]
+//] 
+//
+// 进阶: 
+//
+// 
+// 一个直接的解决方案是使用 O(mn) 的额外空间，但这并不是一个好的解决方案。 
+// 一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。 
+// 你能想出一个常数空间的解决方案吗？ 
+// 
+// Related Topics 数组 
+// 👍 358 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class SetMatrixZerosSolution {
+    public void setZeroes(int[][] matrix) {
+        boolean rowFlag = false;
+        boolean colFlag = false;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                rowFlag = true;
+                break;
+            }
+        }
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                colFlag = true;
+                break;
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            for (int j = matrix[i].length - 1; j >= 0; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (rowFlag) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (colFlag) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
