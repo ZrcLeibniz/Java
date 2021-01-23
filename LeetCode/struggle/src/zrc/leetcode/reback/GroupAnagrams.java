c67cf67ab651ac853d3ef08b1d839d5db528da04
ZrcLeibniz <2834511920@qq.com>
2021-01-23 13:56:34 +0800

    LeetCode 49 GroupAnagrams

---
+++ LeetCode/struggle/src/zrc/leetcode/reback/GroupAnagrams.java
@@ -0,0 +1,46 @@
+package zrc.leetcode.reback;
+
+//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。 
+//
+// 示例: 
+//
+// 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//] 
+//
+// 说明： 
+//
+// 
+// 所有输入均为小写字母。 
+// 不考虑答案输出的顺序。 
+// 
+// Related Topics 哈希表 字符串 
+// 👍 637 👎 0
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class GroupAnagramsSolution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] value = str.toCharArray();
+            Arrays.sort(value);
+            String temp = String.valueOf(value);
+            if (!map.containsKey(temp)) {
+                map.put(temp, new ArrayList<String>());
+            }
+            map.get(temp).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
