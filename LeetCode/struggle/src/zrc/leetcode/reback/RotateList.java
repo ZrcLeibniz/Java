4a924a25c52391094a26b59545921157750b9faa
ZrcLeibniz <2834511920@qq.com>
2021-01-26 15:22:25 +0800

    LeetCode 61 RotateList

---
+++ LeetCode/struggle/src/zrc/leetcode/reback/RotateList.java
@@ -0,0 +1,81 @@
+package zrc.leetcode.reback;
+
+//给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。 
+//
+// 示例 1: 
+//
+// 输入: 1->2->3->4->5->NULL, k = 2
+//输出: 4->5->1->2->3->NULL
+//解释:
+//向右旋转 1 步: 5->1->2->3->4->NULL
+//向右旋转 2 步: 4->5->1->2->3->NULL
+// 
+//
+// 示例 2: 
+//
+// 输入: 0->1->2->NULL, k = 4
+//输出: 2->0->1->NULL
+//解释:
+//向右旋转 1 步: 2->0->1->NULL
+//向右旋转 2 步: 1->2->0->NULL
+//向右旋转 3 步: 0->1->2->NULL
+//向右旋转 4 步: 2->0->1->NULL 
+// Related Topics 链表 双指针 
+// 👍 401 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class RotateListSolution {
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        ListNode circle = head;
+        int num = 1;
+        while (circle.next != null) {
+            circle = circle.next;
+            num++;
+        }
+        ListNode fast = head;
+        circle = head;
+        int pre = k % num;
+        while (pre != 0) {
+            fast = fast.next;
+            pre--;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            circle = circle.next;
+        }
+        fast.next = head;
+        head = circle.next;
+        circle.next = null;
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
