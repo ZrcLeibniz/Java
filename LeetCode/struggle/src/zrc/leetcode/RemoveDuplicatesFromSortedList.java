3bbddfa3505d5ad02d840f515c64699136da5a56
ZrcLeibniz <2834511920@qq.com>
2021-01-27 20:24:45 +0800

    RemoveDuplicatesFromSortedList

---
+++ LeetCode/struggle/src/zrc/leetcode/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,65 @@
+package zrc.leetcode;
+
+//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。 
+//
+// 示例 1: 
+//
+// 输入: 1->1->2
+//输出: 1->2
+// 
+//
+// 示例 2: 
+//
+// 输入: 1->1->2->3->3
+//输出: 1->2->3 
+// Related Topics 链表 
+// 👍 458 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class RemoveDuplicatesFromSortedListSolution {
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode root = head;
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur = cur.next;
+            } else {
+                cur = cur.next;
+                root.next = cur;
+                root = cur;
+            }
+        }
+        root.next = null;
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
