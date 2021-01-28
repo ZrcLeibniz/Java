6345ed3ca965e95ef57deaa11ec294ed34509789
ZrcLeibniz <2834511920@qq.com>
2021-01-28 16:21:39 +0800

    2020 算法3

---
+++ Practice/src/zrc/course/Algorithm3.java
@@ -0,0 +1,118 @@
+package zrc.course;
+
+import java.util.PriorityQueue;
+
+public class Algorithm3 {
+}
+
+// 堆 O(logN)
+class Heap {
+    public void headSort(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, 0);
+        }
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+        while (heapSize > 0) {
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
+    }
+
+    public void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2] && index > 0) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public void heapify(int[] arr, int index, int heapSize) {
+        int left = index * 2 + 1;
+        while (left < heapSize) {
+            int large = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+            large = arr[large] > arr[index] ? large : index;
+            if (large == index) {
+                break;
+            }
+            swap(arr, large, index);
+            index = large;
+            left = index * 2 + 1;
+        }
+    }
+}
+
+// 堆排序扩展题目
+// 已知一个几乎有序的数组，几乎有序是指如果数组排好顺序的话，每个元素移动的距离不超过K，并且K远远小于数组长度。
+// 请对该数组排序，并尽可能选择效率较高的算法
+class HeapExtend {
+    public void sortedArrDistanceLessK(int[] arr, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        int index = 0;
+        for (; index <= Math.min(arr.length, k); index++) {
+            heap.add(arr[index]);
+        }
+        int i = 0;
+        for (; index < arr.length; i++, index++) {
+            heap.add(arr[index]);
+            arr[i] = heap.poll();
+        }
+        while (!heap.isEmpty()) {
+            arr[i++] = heap.poll();
+        }
+    }
+}
+
+class RadixSort {
+    public void radixSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        radixSort(arr, 0, arr.length - 1, maxBits(arr));
+    }
+
+    private void radixSort(int[] arr, int L, int R, int digit) {
+        final int radix = 10;
+        int i = 0;
+        int j = 0;
+        int[] bucket = new int[R - L + 1];
+        for (int d = 1; d <= digit; d++) {
+            int[] count = new int[radix];
+            for (i = L; i <= R; i++) {
+                j = getDigit(arr[i], d);
+                count[j]++;
+            }
+            for (i = 1; i < radix; i++) {
+                count[i] = count[i] + count[i - 1];
+            }
+            for (i = R; i >= L; i--) {
+                j = getDigit(arr[i], d);
+                bucket[count[j] - 1] = arr[j];
+                count[j]--;
+            }
+            for (i = L, j = 0; i <= R; i++, j++) {
+                arr[i] = bucket[j];
+            }
+        }
+    }
+
+    private int getDigit(int x, int d) {
+        return ((x / ((int) Math.pow(10, d - 1))) % 10);
+    }
+
+    public static int maxBits(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        return String.valueOf(max).length();
+    }
+}
