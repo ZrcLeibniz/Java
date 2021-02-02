41f63460125583e6a822529a8331545477809a67
ZrcLeibniz <2834511920@qq.com>
2021-02-02 15:32:52 +0800

    根据给定目标值将链表做出划分，切不改变元素相对顺序

---
+++ Practice/src/zrc/course/algorithm4/PartLinkedList.java
@@ -0,0 +1,67 @@
+package zrc.course.algorithm4;
+
+public class PartLinkedList {
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode solution(ListNode head, int pivot) {
+        ListNode sH = null;
+        ListNode sT = null;
+        ListNode eH = null;
+        ListNode eT = null;
+        ListNode mH = null;
+        ListNode mT = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = null;
+            if (head.val < pivot) {
+                if (sH == null) {
+                    sH = head;
+                    sT = head;
+                } else {
+                    sT.next = head;
+                    sT = sT.next;
+                }
+            } else if (head.val == pivot) {
+                if (eH == null) {
+                    eH = head;
+                    eT = head;
+                } else {
+                    eT.next = head;
+                    eT = eT.next;
+                }
+            } else {
+                if (mH == null) {
+                    mH = head;
+                    mT = head;
+                } else {
+                    mT.next = head;
+                    mT = mT.next;
+                }
+            }
+            head = next;
+        }
+        if (sT != null) {
+            sT.next = eH;
+            eT = eT == null ? sT : eT;
+        }
+        if (eT != null) {
+            eT.next = mH;
+        }
+        return sH != null ? sH : (eH != null ? eH : mH);
+    }
+}
