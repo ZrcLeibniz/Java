2af9641d373a5d3a8010d36c0e278a4728c7a746
ZrcLeibniz <2834511920@qq.com>
2021-02-03 16:08:49 +0800

    算法2020 5 链表补充部分

---
+++ Practice/src/zrc/course/Algorithm5_1.java
@@ -0,0 +1,142 @@
+package zrc.course;
+
+public class Algorithm5_1 {
+
+}
+
+// 两个单链表相交的一系列问题
+// 给定两个可能有环也可能无环的单链表，头节点head1和head2.
+// 请实现一个函数，如果两个链表相交，请返回相交的第一个节点，如果不想交，请返回null
+// 如果两个链表长度之和为N，时间复杂度请达到O(N)，额外空间复杂度请达到O(1)
+class IsIntersectLinkedList {
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // 第一步：判断一个链表有环还是无环,如果有环请返回入环的节点，无环的话请返回null
+    public ListNode getLoopNode(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        ListNode fast = head.next.next;
+        ListNode slow = head.next;
+        while (fast != slow) {
+            if (fast.next == null || fast.next.next == null) {
+                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    // 第一种情况：当两个链表都无果相交返回相交的第一个节点，否则返回null
+    public ListNode noLoop(ListNode head1, ListNode head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        ListNode cur1 = head1;
+        ListNode cur2 = head2;
+        int n = 0;
+        while (cur1.next != null) {
+            n++;
+            cur1 = cur1.next;
+        }
+        while (cur2.next != null) {
+            n--;
+            cur2 = cur2.next;
+        }
+        if (cur1 != cur2) {
+            return null;
+        }
+        cur1 = n > 0 ? head1 : head2;
+        cur2 = cur1 == head1 ? head2 : head1;
+        n = Math.abs(n);
+        while (n != 0) {
+            n--;
+            cur1 = cur1.next;
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+
+    // 第二种情况：一个链表有环另一个链表无环的情况返回相交的第一个节点，否则返回null
+    // 代码不需要写了，因为这样不可能相交
+
+    // 第三种情况：两个链表都有环，返回相交的第一个节点，否则返回null
+    public ListNode bothLoop(ListNode head1, ListNode loop1, ListNode head2, ListNode loop2) {
+        ListNode cur1 = null;
+        ListNode cur2 = null;
+        if (loop1 == loop2) {
+            cur1 = head1;
+            cur2 = head2;
+            int n = 0;
+            while (cur1 != loop1) {
+                n++;
+                cur1 = cur1.next;
+            }
+            while (cur2 != loop2) {
+                n--;
+                cur2 = cur2.next;
+            }
+            cur1 = n > 0 ? head1 : head2;
+            cur2 = cur1 == head1 ? head2 : head1;
+            n = Math.abs(n);
+            while (n != 0) {
+                n--;
+                cur1 = cur1.next;
+            }
+            while (cur1 != cur2) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        } else {
+            cur1 = loop1.next;
+            while (cur1 != loop1) {
+                if (cur1 == loop2) {
+                    return loop1;
+                }
+                cur1 = cur1.next;
+            }
+            return null;
+        }
+    }
+
+    // 将上面所有的情况合并起来
+    public ListNode getIntersectNode(ListNode head1, ListNode head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        ListNode loop1 = getLoopNode(head1);
+        ListNode loop2 = getLoopNode(head2);
+        if (loop1 == null && loop2 == null) {
+            return noLoop(head1, head2);
+        }
+        if (loop1 != null && loop2 != null) {
+            return bothLoop(head1, loop1, head2, loop2);
+        }
+        return null;
+    }
+}
