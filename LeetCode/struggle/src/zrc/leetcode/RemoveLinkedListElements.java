dc7ea7de26461c02b6f2554640d93ecfb1717d8c
ZrcLeibniz <2834511920@qq.com>
2021-02-04 19:37:05 +0800

    RemoveLinkedListElements

---
+++ LeetCode/struggle/src/zrc/leetcode/RemoveLinkedListElements.java
@@ -0,0 +1,56 @@
+package zrc.leetcode;
+
+//删除链表中等于给定值 val 的所有节点。 
+//
+// 示例: 
+//
+// 输入: 1->2->6->3->4->5->6, val = 6
+//输出: 1->2->3->4->5
+// 
+// Related Topics 链表 
+// 👍 524 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class RemoveLinkedListElementsSolution {
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+                cur = cur.next;
+            } else {
+                pre = pre.next;
+                cur = cur.next;
+            }
+        }
+        return head.val == val ? head.next : head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
