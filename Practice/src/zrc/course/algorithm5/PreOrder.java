3405dae7cc632c91f4b045c4f663fc7a222e0996
ZrcLeibniz <2834511920@qq.com>
2021-02-05 14:56:23 +0800

    二叉树的两种前序遍历

---
+++ Practice/src/zrc/course/algorithm5/PreOrder.java
@@ -0,0 +1,45 @@
+package zrc.course.algorithm5;
+
+import java.util.Stack;
+
+public class PreOrder {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode() {
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public void preOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public void preOrderII(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            System.out.print(cur.val + " ");
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+    }
+}
