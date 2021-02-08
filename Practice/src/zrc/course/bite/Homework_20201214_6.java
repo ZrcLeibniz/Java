c5135ffad9304d2b3507bb325ea71bd4955d1ae8
ZrcLeibniz <2834511920@qq.com>
2021-02-08 10:33:27 +0800

    Test

---
+++ Practice/src/zrc/course/bite/Homework_20201214_6.java
@@ -0,0 +1,30 @@
+package zrc.course.bite;
+
+import zrc.linear.Stack;
+
+public class Homework_20201214_6 {
+    public static void main(String[] args) {
+        Homework_20201214_6 test = new Homework_20201214_6();
+        System.out.println(test.printPosition(472937));
+    }
+
+    public StringBuilder printPosition (int i) {
+    }
+    Stack<Integer> stack = new Stack<>();
+        if (i == 0) {
+            return new StringBuilder("0");
+        while (i != 0) {
+            stack.push(i % 10);
+            i = i / 10;
+        }
+        StringBuilder result = new StringBuilder();
+        result.append("{");
+        for (Integer integer : stack) {
+            result.append(integer);
+            result.append(",");
+        }
+        result.deleteCharAt(result.length() - 1);
+        result.append("}");
+        return result;
+    }
+}
