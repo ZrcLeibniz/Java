8745b369176433ea1c44f73790e0eff1347b9e19
ZrcLeibniz <2834511920@qq.com>
2021-02-18 13:43:21 +0800

    图的深度优先搜索

---
+++ Practice/src/zrc/course/algorithm7/DepthFirstSearch.java
@@ -0,0 +1,26 @@
+package zrc.course.algorithm7;
+
+import java.util.HashSet;
+import java.util.Stack;
+
+public class DepthFirstSearch {
+    public void dfs(Node node) {
+        Stack<Node> stack = new Stack<>();
+        HashSet<Node> set = new HashSet<>();
+        stack.add(node);
+        set.add(node);
+        System.out.println(node.value);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    System.out.println(next.value);
+                    stack.push(cur);
+                    stack.push(next);
+                    set.add(next);
+                    break;
+                }
+            }
+        }
+    }
+}
