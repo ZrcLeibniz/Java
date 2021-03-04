29e2af74f072f1eb5358ae25cc3edc511010270b
ZrcLeibniz <2834511920@qq.com>
2021-03-04 14:20:39 +0800

    合并两个有序链表

---
+++ Practice/src/zrc/course/bite/linkedlist/MergeTwoSortedLists.java
@@ -0,0 +1,31 @@
+package zrc.course.bite.linkedlist;
+
+public class MergeTwoSortedLists {
+    private class ListNode {
+        int val;
+        ListNode next;
+    }
+
+    public ListNode solution(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+        ListNode result = new ListNode();
+        ListNode cur = result;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return result.next;
+    }
+}
