844d453fcbfe9be2df1347c5a4285d14b5861fe1
ZrcLeibniz <2834511920@qq.com>
2021-03-17 16:55:38 +0800

    ValidateBinarySearchTree

---
+++ LeetCode/struggle/src/zrc/leetcode/reback/ValidateBinarySearchTree.java
@@ -0,0 +1,110 @@
+package zrc.leetcode.reback;
+
+//给定一个二叉树，判断其是否是一个有效的二叉搜索树。 
+//
+// 假设一个二叉搜索树具有如下特征： 
+//
+// 
+// 节点的左子树只包含小于当前节点的数。 
+// 节点的右子树只包含大于当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 示例 1: 
+//
+// 输入:
+//    2
+//   / \
+//  1   3
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入:
+//    5
+//   / \
+//  1   4
+//     / \
+//    3   6
+//输出: false
+//解释: 输入为: [5,1,4,null,null,3,6]。
+//     根节点的值为 5 ，但是其右子节点值为 4 。
+// 
+// Related Topics 树 深度优先搜索 递归 
+// 👍 961 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class ValidateBinarySearchTreeSolution {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    private static class Info {
+        int min;
+        int max;
+        boolean isBST;
+
+        public Info(int min, int max, boolean isBST) {
+            this.min = min;
+            this.max = max;
+            this.isBST = isBST;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return process(root).isBST;
+    }
+
+    private Info process(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Info leftInfo = process(root.left);
+        Info rightInfo = process(root.right);
+        int min = root.val;
+        int max = root.val;
+        if (leftInfo != null) {
+            min = Math.min(min, leftInfo.min);
+            max = Math.max(max, leftInfo.max);
+        }
+        if (rightInfo != null) {
+            min = Math.min(min, rightInfo.min);
+            max = Math.max(max, rightInfo.max);
+        }
+        boolean isBST = true;
+        if (leftInfo != null && (leftInfo.max > root.val || !leftInfo.isBST)) {
+            isBST = false;
+        }
+        if (rightInfo != null && (rightInfo.max > root.val || !rightInfo.isBST)) {
+            isBST = false;
+        }
+        return new Info(min, max, isBST);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+
+
