82a1ef7422e99548c3095befe0fdf58264140744
ZrcLeibniz <2834511920@qq.com>
2021-03-17 17:40:36 +0800

    BinaryTreeZigzagLevelOrderTraversal

---
+++ LeetCode/struggle/src/zrc/leetcode/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,86 @@
+package zrc.leetcode;
+
+//给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。 
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回锯齿形层序遍历如下： 
+//
+// 
+//[
+//  [3],
+//  [20,9],
+//  [15,7]
+//]
+// 
+// Related Topics 栈 树 广度优先搜索 
+// 👍 412 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class BinaryTreeZigzagLevelOrderTraversalSolution {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean flag = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Deque<Integer> level = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (flag) {
+                    level.addLast(cur.val);
+                } else {
+                    level.addFirst(cur.val);
+                }
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+            result.add(new LinkedList<Integer>(level));
+            flag = !flag;
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
