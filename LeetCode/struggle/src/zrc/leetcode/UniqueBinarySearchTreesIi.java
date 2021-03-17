4ef6262dfffd32cc7cd17b303f1443815aad6a54
ZrcLeibniz <2834511920@qq.com>
2021-03-17 16:18:03 +0800

    UniqueBinarySearchTreesII

---
+++ LeetCode/struggle/src/zrc/leetcode/UniqueBinarySearchTreesIi.java
@@ -0,0 +1,99 @@
+package zrc.leetcode;
+
+//给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：3
+//输出：
+//[
+//  [1,null,3,2],
+//  [3,2,null,1],
+//  [3,1,null,null,2],
+//  [2,1,3],
+//  [1,null,2,null,3]
+//]
+//解释：
+//以上的输出对应以下 5 种不同结构的二叉搜索树：
+//
+//   1         3     3      2      1
+//    \       /     /      / \      \
+//     3     2     1      1   3      2
+//    /     /       \                 \
+//   2     1         2                 3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= n <= 8 
+// 
+// Related Topics 树 动态规划 
+// 👍 812 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class UniqueBinarySearchTreesIISolution {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        List<TreeNode> result = new LinkedList<>();
+        if (n == 0) {
+            return result;
+        }
+        return process(0, n);
+    }
+
+    private List<TreeNode> process(int begin, int end) {
+        List<TreeNode> allTrees = new LinkedList<>();
+        if (begin > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+        for (int i = begin; i <= end; i++) {
+            List<TreeNode> leftTrees = process(begin, i - 1);
+            List<TreeNode> rightTrees = process(i + 1, end);
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    allTrees.add(root);
+                }
+            }
+        }
+        return allTrees;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
