ed45bdea6ddc0a0a32f3cb60a37f8ab26a7495e4
ZrcLeibniz <2834511920@qq.com>
2021-03-19 12:19:20 +0800

    BinaryTreeLevelOrderTraversalII

---
+++ LeetCode/struggle/src/zrc/leetcode/BinaryTreeLevelOrderTraversalIi.java
@@ -0,0 +1,84 @@
+package zrc.leetcode;
+
+//给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历） 
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其自底向上的层序遍历为： 
+//
+// 
+//[
+//  [15,7],
+//  [9,20],
+//  [3]
+//]
+// 
+// Related Topics 树 广度优先搜索 
+// 👍 421 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BinaryTreeLevelOrderTraversalIISolution {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        Stack<List<Integer>> stack = new Stack<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                level.add(cur.val);
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+            stack.push(level);
+        }
+        while (!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
