2567490f8913e3558e83c01a37dded955c708d71
ZrcLeibniz <2834511920@qq.com>
2021-03-23 11:14:58 +0800

    EditDistance

---
+++ LeetCode/struggle/src/zrc/leetcode/EditDistance.java
@@ -0,0 +1,79 @@
+package zrc.leetcode;
+
+//给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。 
+//
+// 你可以对一个单词进行如下三种操作： 
+//
+// 
+// 插入一个字符 
+// 删除一个字符 
+// 替换一个字符 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：word1 = "horse", word2 = "ros"
+//输出：3
+//解释：
+//horse -> rorse (将 'h' 替换为 'r')
+//rorse -> rose (删除 'r')
+//rose -> ros (删除 'e')
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：word1 = "intention", word2 = "execution"
+//输出：5
+//解释：
+//intention -> inention (删除 't')
+//inention -> enention (将 'i' 替换为 'e')
+//enention -> exention (将 'n' 替换为 'x')
+//exention -> exection (将 'n' 替换为 'c')
+//exection -> execution (插入 'u')
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= word1.length, word2.length <= 500 
+// word1 和 word2 由小写英文字母组成 
+// 
+// Related Topics 字符串 动态规划 
+// 👍 1486 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class EditDistanceSolution {
+    public int minDistance(String word1, String word2) {
+        int col = word2.length() + 1;
+        int row = word1.length() + 1;
+        int[][] dp = new int[row][col];
+        char[] chars1 = word1.toCharArray();
+        char[] chars2 = word2.toCharArray();
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (chars1[i - 1] == chars2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
