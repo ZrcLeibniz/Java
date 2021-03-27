3f4f690119bc828be5a64e0f18ebf59e639eed7f
ZrcLeibniz <2834511920@qq.com>
2021-03-27 10:53:51 +0800

    JewelsAndStones

---
+++ LeetCode/struggle/src/leetcode/editor/cn/JewelsAndStones.java
@@ -0,0 +1,48 @@
+package leetcode.editor.cn;
+
+//给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。 
+//
+// J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。 
+//
+// 示例 1: 
+//
+// 输入: J = "aA", S = "aAAbbbb"
+//输出: 3
+// 
+//
+// 示例 2: 
+//
+// 输入: J = "z", S = "ZZ"
+//输出: 0
+// 
+//
+// 注意: 
+//
+// 
+// S 和 J 最多含有50个字母。 
+// J 中的字符不重复。 
+// 
+// Related Topics 哈希表 
+// 👍 634 👎 0
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class JewelsAndStonesSolution {
+    public int numJewelsInStones(String jewels, String stones) {
+        char[] chars = jewels.toCharArray();
+        Set<Character> set = new HashSet<>();
+        for (char c : chars) {
+            set.add(c);
+        }
+        chars = stones.toCharArray();
+        int result = 0;
+        for (char c : chars) {
+            result = set.contains(c) ? result + 1 : result;
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
