9b669059fe5da9a10f33ff1d3413280e12fd3e02
ZrcLeibniz <2834511920@qq.com>
2021-03-31 10:35:59 +0800

    WordBreak

---
+++ LeetCode/struggle/src/leetcode/editor/cn/WordBreak.java
@@ -0,0 +1,60 @@
+package leetcode.editor.cn;
+
+//给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。 
+//
+// 说明： 
+//
+// 
+// 拆分时可以重复使用字典中的单词。 
+// 你可以假设字典中没有重复的单词。 
+// 
+//
+// 示例 1： 
+//
+// 输入: s = "leetcode", wordDict = ["leet", "code"]
+//输出: true
+//解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+// 
+//
+// 示例 2： 
+//
+// 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+//输出: true
+//解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+//     注意你可以重复使用字典中的单词。
+// 
+//
+// 示例 3： 
+//
+// 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+//输出: false
+// 
+// Related Topics 动态规划 
+// 👍 905 👎 0
+
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class WordBreakSolution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length()];
+        for (int right = 0; right < s.length(); right++) {
+            if (wordSet.contains(s.substring(0, right + 1))) {
+                dp[right] = true;
+                continue;
+            }
+            for (int left = right - 1; left >= 0; left--) {
+                if (wordSet.contains(s.substring(left + 1, right + 1)) && dp[left]) {
+                    dp[right] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length() - 1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
