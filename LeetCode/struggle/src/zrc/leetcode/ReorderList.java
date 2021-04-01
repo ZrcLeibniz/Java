f5d368f4a59f51f1469603ca2d4e47323ecd4341
ZrcLeibniz <2834511920@qq.com>
2021-04-01 09:51:09 +0800

    ReorderList

---
+++ LeetCode/struggle/src/zrc/leetcode/ReorderList.java
@@ -0,0 +1,86 @@
+package zrc.leetcode;
+
+//给定一个单链表 L：L0→L1→…→Ln-1→Ln ， 
+//将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→… 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 示例 1: 
+//
+// 给定链表 1->2->3->4, 重新排列为 1->4->2->3. 
+//
+// 示例 2: 
+//
+// 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3. 
+// Related Topics 链表 
+// 👍 548 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return;
+        }
+        ListNode l1 = head;
+        ListNode midNode = getMidNode(head);
+        ListNode l2 = midNode.next;
+        midNode.next = null;
+        l2 = reverse(l2);
+        mergeList(l1, l2);
+    }
+
+    private void mergeList(ListNode l1, ListNode l2) {
+        while (l1 != null && l2 != null) {
+            ListNode nextI = l1.next;
+            ListNode nextII = l2.next;
+            l2.next = l1.next;
+            l1.next = l2;
+            l1 = nextI;
+            l2 = nextII;
+        }
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    private ListNode getMidNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
