14884f4f56ca0ab60ae094841b245e2ccfb1840e
ZrcLeibniz <2834511920@qq.com>
2021-04-02 14:08:05 +0800

    SortList

---
+++ LeetCode/struggle/src/zrc/leetcode/SortList.java
@@ -0,0 +1,122 @@
+package zrc.leetcode;
+
+//给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。 
+//
+// 进阶： 
+//
+// 
+// 你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？ 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [4,2,1,3]
+//输出：[1,2,3,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [-1,5,3,4,0]
+//输出：[-1,0,3,4,5]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 5 * 104] 内 
+// -105 <= Node.val <= 105 
+// 
+// Related Topics 排序 链表 
+// 👍 1070 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+//leetcode submit region end(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class SortListSolution {
+    private static class ListNode {
+        int val;
+        ListNode next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        PriorityQueue<ListNode> priorityQueue = new PriorityQueue<>(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = null;
+            priorityQueue.add(head);
+            head = head.next;
+        }
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while (!priorityQueue.isEmpty()) {
+            cur.next = priorityQueue.poll();
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
+    public ListNode sortListII(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null;
+        ListNode left = sortListII(head);
+        ListNode right = sortListII(mid);
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                cur.next = left;
+                left = left.next;
+            } else {
+                cur.next = right;
+                right = right.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = left == null ? right : left;
+        return dummy.next;
+    }
+}
