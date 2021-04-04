c78161f414a2dd030e564ef4e997d6e09eafea35
ZrcLeibniz <2834511920@qq.com>
2021-04-04 17:17:03 +0800

    ReverserWordsAString

--- LeetCode/struggle/src/Test.java
+++ LeetCode/struggle/src/Test.java
@@ -1,49 +1,13 @@
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.*;
 
 
 public class Test {
-    public static boolean containsNearbyDuplicate(int[] nums, int k) {
-        //HashSet<Integer> hashSet = new HashSet<Integer>();
-        ArrayList<Integer> hashSet = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (hashSet.contains(nums[i])) {
-                return true;
-            }
-            hashSet.add(nums[i]);
-            if (hashSet.size() > k) {
-                hashSet.remove(nums[i - k]);
-            }
-        }
-        return false;
-    }
-
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 1, 2, 3};
-        boolean b = containsNearbyDuplicate(nums, 2);
-        System.out.println(b);
-        String s = "abcde";
-        String substring = s.substring(0, 3);
-        System.out.println(s);
-        System.out.println(substring);
-    }
-}
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-
-    @Override
-    public String toString() {
-        return "TreeNode{" +
-                "val=" + val +
-                ", left=" + left +
-                ", right=" + right +
-                '}';
+        Set<String> set = new HashSet<>();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            set.add(scanner.next());
+        }
+        System.out.println(set.size());
     }
 }
