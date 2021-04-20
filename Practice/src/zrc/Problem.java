13e0db85b0781fc79004c6e185df7a9cf1557e44
ZrcLeibniz <2834511920@qq.com>
2021-04-20 10:21:16 +0800

    每日一题 20210420-1

--- Practice/src/zrc/Problem.java
+++ Practice/src/zrc/Problem.java
@@ -4,28 +4,5 @@ package zrc;
 import java.util.Scanner;
 
 public class Problem {
-    private static int solution(int A, int B) {
-        int max = Math.max(A, B);
-        int min = Math.min(A, B);
-        if (max % min == 0) {
-            return max;
-        }
-        int result = max;
-        while (true) {
-            if (result % max != 0 || result % min != 0) {
-                result++;
-            } else {
-                break;
-            }
-        }
-        return result;
-    }
 
-    public static void main(String[] args) {
-        Scanner scanner  = new Scanner(System.in);
-        int i = scanner.nextInt();
-        int j = scanner.nextInt();
-        int solution = solution(i, j);
-        System.out.println(solution);
-    }
 }
