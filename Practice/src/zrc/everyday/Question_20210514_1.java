76779ec145c63b5ebc4d2b3cc9816f8cb33bd801
ZrcLeibniz <2834511920@qq.com>
2021-05-14 13:16:54 +0800

    每日一题 20210514-1

--- Practice/src/zrc/everyday/Question_20210514_1.java
+++ Practice/src/zrc/everyday/Question_20210514_1.java
@@ -1,8 +1,8 @@
-package zrc;
+package zrc.everyday;
 
-import java.util.*;
+import java.util.HashMap;
 
-public class Problem {
+public class Question_20210514_1 {
     public static int solution(int[] gifts, int n) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int gift : gifts) {
@@ -17,11 +17,5 @@ public class Problem {
             }
         }
         return 0;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 2, 2};
-        int solution = solution(arr, 5);
-        System.out.println(solution);
     }
 }
