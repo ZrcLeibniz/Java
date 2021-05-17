57538fb46d05ce434a91f5d499005ff6f7a5ef85
ZrcLeibniz <2834511920@qq.com>
2021-05-17 09:56:42 +0800

    每日一题 20210517-2

--- Practice/src/zrc/Problem.java
+++ Practice/src/zrc/Problem.java
@@ -1,7 +1,30 @@
 package zrc;
 
-import java.util.*;
-import java.util.List;
+
+import java.util.Scanner;
 
 public class Problem {
+     public static String solution(String num) {
+         while (num.length() != 1) {
+             num = process(num);
+         }
+         return num;
+     }
+
+     private static String process(String num) {
+         int result = 0;
+         for (int i = 0; i < num.length(); i++) {
+             result += num.charAt(i) - '0';
+         }
+         return Integer.toString(result);
+     }
+
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         while (scanner.hasNext()) {
+             String num = scanner.next();
+             String solution = solution(num);
+             System.out.println(solution);
+         }
+    }
 }
---
+++ Practice/src/zrc/everyday/Question_20210517_2.java
@@ -0,0 +1,29 @@
+package zrc.everyday;
+
+import java.util.Scanner;
+
+public class Question_20210517_2 {
+    public static String solution(String num) {
+        while (num.length() != 1) {
+            num = process(num);
+        }
+        return num;
+    }
+
+    private static String process(String num) {
+        int result = 0;
+        for (int i = 0; i < num.length(); i++) {
+            result += num.charAt(i) - '0';
+        }
+        return Integer.toString(result);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String num = scanner.next();
+            String solution = solution(num);
+            System.out.println(solution);
+        }
+    }
+}
